bfa117ec8f1e303f1d419082708a1fba84a7c417
Anne-Julie GOUBO <annadevri46@gmail.com>
2025-07-10 15:38:02 +0200

    Tentative de mis en place de la page connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login-form/login-form';
 
 export const routes: Routes = [
-    
+  { path: 'login-form', component: LoginComponent },
+  { path: '', redirectTo: 'login-form', pathMatch: 'full' }, // redirection par défaut
 ];
